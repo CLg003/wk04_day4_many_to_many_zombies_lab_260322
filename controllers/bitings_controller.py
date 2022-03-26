d2da89c99b4e6a973fabcaf385e0ead2d293d5de
CLg003 <clairemjlaing@gmail.com>
2022-03-26 21:27:33 +0000

    Bitings create route.

--- controllers/bitings_controller.py
+++ controllers/bitings_controller.py
@@ -2,6 +2,8 @@ from flask import Blueprint, Flask, redirect, render_template, request
 
 from models.biting import Biting
 import repositories.biting_repository as biting_repository
+import repositories.human_repository as human_repository
+import repositories.zombie_repository as zombie_repository
 
 bitings_blueprint = Blueprint("bitings", __name__)
 
@@ -13,10 +15,22 @@ def bitings():
 
 # NEW
 @bitings_blueprint.route('/bitings/new')
-def new():
-    return render_template('bitings/new.html')
+def new_bitings():
+    humans = human_repository.select_all()
+    zombies = zombie_repository.select_all()
+    return render_template('bitings/new.html', humans=humans, zombies=zombies)
 
 # CREATE
+@bitings_blueprint.route('/bitings', methods=['POST'])
+def create_biting():
+    human_id = request.form['human_id']
+    zombie_id = request.form['zombie_id']
+    human = human_repository.select(human_id)
+    zombie = zombie_repository.select(zombie_id)
+    new_biting = Biting(human, zombie)
+    biting_repository.save(new_biting)
+    return redirect('/bitings')
+
 
 # EDIT
 
